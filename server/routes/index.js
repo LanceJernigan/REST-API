3bc723d336ab3d1976ee54875dce2c270e1c3020
LanceJernigan <Lance@LanceJernigan.com>
2018-02-09 15:02:16 -0500

    initial resource routing setup

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,17 +1,29 @@
 const Router = require('koa-router');
 const router = new Router();
 
-router.get('/', ctx => {
+router.get('/:resource', ctx => {
+    const {
+        resource = false
+    } = ctx.params;
+
     ctx.body = {
         status: 'success',
         message: 'Hello world!',
+        resource: resource,
     }
 });
 
-router.post('/', ctx => {
+router.get('/:resource/:id', ctx => {
+    const {
+        resource = false,
+        id = 0
+    } = ctx.params;
+
     ctx.body = {
         status: 'success',
-        message: 'Goodbye world!',
+        message: 'Hello world!',
+        resource: resource,
+        id: id,
     }
 });
 
